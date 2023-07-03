fe22a0f2ca438650b61cb950b38365c3c4cb5e58
nieshxa <niknakriego@gmail.com>
2023-07-03 13:31:53 +0800

    services modal fixed

--- js/modal.js
+++ js/modal.js
@@ -2,11 +2,17 @@ function openModal(title, description) {
   var modal = document.getElementById("modal");
   var modalTitle = document.getElementById("modal-title");
   var modalDescription = document.getElementById("modal-description");
+  var closeButton = document.getElementsByClassName("close")[0]; // Get the closing button element
 
   modalTitle.innerText = title;
   modalDescription.innerText = description;
 
   modal.style.display = "flex";
+
+  // Add an event listener to the closing button
+  closeButton.onclick = function () {
+    modal.style.display = "none";
+  };
 }
 
 window.onclick = function (event) {
