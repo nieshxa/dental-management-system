ff6e0f7461967e382a1854112b23919b9ebc89af
nieshxa <niknakriego@gmail.com>
2023-07-05 17:30:41 +0800

    appt tables complete changed

--- js/searchTable-appointments.js
+++ js/searchTable-appointments.js
@@ -1,34 +1,52 @@
-$("#search-appt").keyup(function () {
-  var searchValue = $(this).val().toLowerCase();
-  var $tableRows = $(".appt-sched.active table tbody tr");
-
-  $tableRows.hide();
-
-  $tableRows.each(function () {
-    var $secondColumn = $(this).find("td:nth-child(2)");
-    var $fourthColumn = $(this).find("td:nth-child(4)");
-
-    if (
-      $secondColumn.text().toLowerCase().includes(searchValue) ||
-      $fourthColumn.text().toLowerCase().includes(searchValue)
-    ) {
-      $(this).show();
-    }
-  });
-
-  var $visibleRows = $(".appt-sched.active table tbody tr:visible");
-
-  if ($visibleRows.length === 0) {
-    $(".appt-sched.active table tbody").append(
-      '<tr class="no-matches"><td colspan="6">No rows matched.</td></tr>'
-    );
-  } else {
-    $(".no-matches").remove();
-  }
-});
+// clear search engine
 $(".clear-search").click(function () {
   $("#search-appt").val("");
 
   $(".appt-sched.active table tbody tr").show();
   $(".no-matches").remove();
+});
+
+// search
+document.getElementById("search-appt").addEventListener("keyup", function () {
+  var input, filter, table, tr, serviceName, serviceDate, i, txtValue;
+  input = document.getElementById("search-appt");
+  filter = input.value.toUpperCase();
+  table = document.querySelector(".appt-sched.active table");
+  tr = table.getElementsByTagName("tr");
+
+  var displayed = false; // Flag to track if any rows were displayed
+
+  for (i = 0; i < tr.length; i++) {
+    serviceName = tr[i].querySelector(".service-name");
+    serviceDate = tr[i].querySelector(".service-date");
+
+    if (serviceName && serviceDate) {
+      txtValue = serviceName.textContent || serviceName.innerText;
+      txtValue += serviceDate.textContent || serviceDate.innerText;
+
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+        displayed = true; // Set the flag to true if a row is displayed
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+
+  // Remove the "No matches found" row if it exists
+  var noMatchRow = table.querySelector(".no-matches-row");
+  if (noMatchRow) {
+    table.removeChild(noMatchRow);
+  }
+
+  // Create a new row if no matches were found
+  if (!displayed && filter !== "") {
+    noMatchRow = document.createElement("tr");
+    noMatchRow.classList.add("no-matches-row");
+    var noMatchCell = document.createElement("td");
+    noMatchCell.setAttribute("colspan", "2");
+    noMatchCell.textContent = "No matches found.";
+    noMatchRow.appendChild(noMatchCell);
+    table.appendChild(noMatchRow);
+  }
 });
