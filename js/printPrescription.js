849509790537f8d8145b58f7e4595dac043f95da
nieshxa <niknakriego@gmail.com>
2023-07-29 08:04:11 +0800

    print prescription

--- js/printPrescription.js
+++ js/printPrescription.js
@@ -1,20 +1,45 @@
-function printRow(button) {
-  var row = button.parentNode.parentNode;
-  var medicationName = row.cells[0].innerText;
-  var type = row.cells[1].innerText;
-  var dosage = row.cells[2].innerText;
-  var quantity = row.cells[3].innerText; 
+// Function to generate PDF for a specific row
+function generatePDF(row) {
+  const printableDiv = document.getElementById("printable-view");
+  printableDiv.style.display = "flex";
+  const medName = row.querySelector("[data-title='Medication']").innerText;
+  const dosage = row.querySelector("[data-title='Dosage']").innerText;
+  const type = row.querySelector("[data-title='Form']").innerText;
+  const quantity = row.querySelector("[data-title='Quantity']").innerText;
 
-  var printableRow = document.querySelector("#printable-view .printable-row");
-  printableRow.querySelector(".med-name").innerText = medicationName;
-  printableRow.querySelector(".dosage").innerText = dosage;
-  printableRow.querySelector(".quantity").innerText = quantity;
-  printableRow.querySelector(".type").innerText = type;
+  const medNameSpan = printableDiv.querySelector(".med-name");
+  const dosageSpan = printableDiv.querySelector(".dosage");
+  const typeSpan = printableDiv.querySelector(".type");
+  const quantitySpan = printableDiv.querySelector(".quantity");
 
-  // Print the row content using window.print()
-  var printableView = document.querySelector("#printable-view").innerHTML;
-  var originalBody = document.body.innerHTML;
-  document.body.innerHTML = printableView;
-  window.print();
-  document.body.innerHTML = originalBody;
+  medNameSpan.innerText = medName;
+  dosageSpan.innerText = dosage;
+  typeSpan.innerText = type;
+  quantitySpan.innerText = quantity;
+
+  // Create PDF from the printable div and save it
+  const opt = {
+    margin: 0,
+    filename: "prescription.pdf",
+    image: { type: "jpeg", quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: "in", format: [6, 7.5], orientation: "portrait" },
+  };
+
+  html2pdf().from(printableDiv).set(opt).save();
 }
+
+// Function to handle button click
+function printRow(button) {
+  const row = button.closest("tr");
+  generatePDF(row);
+}
+// document.addEventListener("DOMContentLoaded", function () {
+//   const closeBtn = document.querySelector(".close");
+//   const printableView = document.querySelector(".printable-view");
+
+//   closeBtn.addEventListener("click", function () {
+//     // Add the 'hide' class to the printable view
+//     printableView.classList.add("hide");
+//   });
+// });
